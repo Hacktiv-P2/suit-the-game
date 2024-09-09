1fc3a8e79315b3ab30d10a553004541add8b0e15
quadrantbs <quadrantbs@gmail.com>
2024-09-09 17:27:57 +0700

    add tampilan Pilihan setelah selesai

--- rock-paper-scissors-multiplayer/src/RockPaperScissorsMultiplayer.jsx
+++ rock-paper-scissors-multiplayer/src/RockPaperScissorsMultiplayer.jsx
@@ -157,11 +157,14 @@ const RockPaperScissorsMultiplayer = () => {
           <p>
             Pemain 1 memilih:{" "}
             {gameData.player1.choice ? "Sudah Memilih" : "Belum memilih"}
+            {gameFinished && <span> (Pilihan: {gameData.player1.choice})</span>}
           </p>
           <p>
             Pemain 2 memilih:{" "}
             {gameData.player2.choice ? "Sudah Memilih" : "Belum memilih"}
+            {gameFinished && <span> (Pilihan: {gameData.player2.choice})</span>}
           </p>
+
           {gameData.player1.choice && gameData.player2.choice && (
             <>
               <p>
