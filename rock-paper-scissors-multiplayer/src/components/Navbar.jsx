e1a50669159d73c45e91ada8f3940b9890e38c31
Fabian Tenzara <fabiantenzara@gmail.com>
2024-09-10 22:46:00 +0700

    feat: add toggle button for dark/light mode in navbar, & implement it on homepage

--- rock-paper-scissors-multiplayer/src/components/Navbar.jsx
+++ rock-paper-scissors-multiplayer/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/Suit_High.png";
+import { useEffect, useState } from "react";
 const Navbar = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
@@ -7,8 +8,27 @@ const Navbar = () => {
     navigate("/login");
   };
 
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("darkMode");
+    return savedTheme === "true";
+  });
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("darkMode", "true");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("darkMode", "false");
+    }
+  }, [isDarkMode]);
+
   return (
-    <nav className="bg-color1 p-4">
+    <nav className="bg-color1 p-4 dark:bg-color2">
       <div className="container mx-auto flex justify-between items-center">
         <Link to={"/"}>
           <img
@@ -18,12 +38,21 @@ const Navbar = () => {
             className="mr-4"
           />
         </Link>
-        <div className="flex-grow"></div>
+        <div className="flex-grow">
+        <li>
+            <button
+              onClick={toggleDarkMode}
+              className="text-color2 hover:text-color4 p-2 rounded dark:text-color4 dark:bg-color2 dark:hover:bg-[#005291]"
+            >
+              {isDarkMode ? "Light Mode" : "Dark Mode"}
+            </button>
+          </li>
+        </div>
         <ul className="flex space-x-4">
           <li>
             <Link
               to={"/"}
-              className="text-color2 hover:text-color4 hover:bg-color2 p-2 rounded"
+              className="text-color2 hover:text-color4 hover:bg-color2 p-2 rounded dark:text-color4 dark:hover:bg-[#005291]"
             >
               Home
             </Link>
@@ -31,7 +60,7 @@ const Navbar = () => {
           <li>
             <Link
               to={"/rooms"}
-              className="text-color2 hover:text-color4 hover:bg-color2 p-2 rounded"
+              className="text-color2 hover:text-color4 hover:bg-color2 p-2 rounded dark:text-color4 dark:hover:bg-[#005291]"
             >
               Rooms
             </Link>
@@ -39,7 +68,7 @@ const Navbar = () => {
           <li>
             <Link
               onClick={handleLogout}
-              className="text-color2 hover:text-color4 cursor-pointer hover:bg-color2 p-2 rounded"
+              className="text-color2 hover:text-color4 cursor-pointer hover:bg-color2 p-2 rounded dark:text-color4 dark:hover:bg-[#870420]"
             >
               Logout
             </Link>
--- rock-paper-scissors-multiplayer/src/pages/Home.jsx
+++ rock-paper-scissors-multiplayer/src/pages/Home.jsx
@@ -4,19 +4,19 @@ import logo from "../assets/Suit_High.png";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800 p-6">
       {/* Hero Section */}
       <div className="text-center items-center justify-center mb-10">
         <img src={logo} style={{ height: "300px" }} className="mx-auto mb-8" />
-        <p className="text-xl text-color2">
+        <p className="text-xl text-color2 dark:text-gray-300">
           Welcome to "Suit the Game" – a multiplayer rock-paper-scissors game!
         </p>
       </div>
 
       {/* Game Description */}
-      <div className="bg-white shadow-lg rounded-lg p-6 max-w-4xl text-center">
-        <h2 className="text-2xl font-semibold text-color1 mb-4">How to Play</h2>
-        <p className="text-lg text-color3 mb-6">
+      <div className="bg-white dark:bg-gray-900 dark:text-gray-200 shadow-lg rounded-lg p-6 max-w-4xl text-center">
+        <h2 className="text-2xl font-semibold text-color1 dark:text-gray-300 mb-4">How to Play</h2>
+        <p className="text-lg text-[#6e1833] dark:text-gray-400 mb-6">
           Rock-paper-scissors is a simple and fun game that everyone can play.
           In this multiplayer version, you can challenge other players in
           real-time! The rules are easy: rock beats scissors, scissors beat
@@ -28,14 +28,15 @@ const Home = () => {
       <div className="mt-10">
         <Link
           to={"/rooms"}
-          className="bg-color2 text-white px-6 py-3 rounded-lg hover:bg-color3"
+          className="bg-color2 text-white dark:bg-gray-700 dark:text-gray-200 px-6 py-3 rounded-lg hover:bg-color3 dark:hover:bg-gray-600"
         >
           Start Playing
         </Link>
       </div>
 
       {/* Footer */}
-      <footer className="mt-12 text-color1">
+      <hr/>
+      <footer className="mt-12 text-color1 dark:text-gray-400">
         <p>© 2024 Suit The Game. All rights reserved.</p>
       </footer>
     </div>
