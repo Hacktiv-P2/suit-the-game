011b390485e9e20892b3bea63805133394a137c2
quadrantbs <quadrantbs@gmail.com>
2024-09-11 18:05:38 +0700

    feat: add swal and toastify

--- rock-paper-scissors-multiplayer/src/pages/Game.jsx
+++ rock-paper-scissors-multiplayer/src/pages/Game.jsx
@@ -10,21 +10,21 @@ import {
   off,
 } from "firebase/database";
 import Swal from "sweetalert2";
+import { ToastContainer, toast } from 'react-toastify';
 
 const Game = () => {
-  const { gameId } = useParams(); // Mengambil gameId dari params
-  const navigate = useNavigate(); // Untuk navigasi setelah game berakhir
-  const db = getDatabase(); // Mengakses Realtime Database Firebase
+  const { gameId } = useParams();
+  const navigate = useNavigate();
+  const db = getDatabase();
   const [gameData, setGameData] = useState(null);
-  const [player, setPlayer] = useState(null); // player1 atau player2
+  const [player, setPlayer] = useState(null);
   const [isReady, setIsReady] = useState(false);
   const [p1Lives, setP1Lives] = useState(3);
   const [p2Lives, setP2Lives] = useState(3);
-  const [countdown, setCountdown] = useState(5); // countdown 10 detik
+  const [countdown, setCountdown] = useState(5);
   const playerName = localStorage.getItem("suit_username");
 
   useEffect(() => {
-    // Mendapatkan data dari Firebase berdasarkan gameId
     const gameRef = ref(db, `games/${gameId}`);
     onValue(gameRef, (snapshot) => {
       const data = snapshot.val();
@@ -34,7 +34,6 @@ const Game = () => {
         setP1Lives(data.player1.lives);
         setP2Lives(data.player2.lives);
 
-        // Jika data sudah ada dan player belum diatur, tentukan player1 atau player2
         if (!player) {
           if (data.player1.name === playerName) {
             setPlayer("player1");
@@ -64,9 +63,9 @@ const Game = () => {
   }, [gameId, player]);
 
   const handleReady = () => {
-    // Mengatur status ready ketika player siap
     update(ref(db, `games/${gameId}/${player}`), { ready: true });
     setIsReady(true);
+    toast.success(`${player} is ready!`);
   };
 
   useEffect(() => {
@@ -79,6 +78,7 @@ const Game = () => {
   const handleChoice = (selectedChoice) => {
     update(ref(db, `games/${gameId}/${player}`), { choice: selectedChoice });
     console.log(`${player} has chosen: ${selectedChoice}`);
+    toast.success(`${player} has chosen: ${selectedChoice}`);
   };
 
   useEffect(() => {
@@ -98,44 +98,68 @@ const Game = () => {
     const { player1, player2 } = gameData;
 
     if (!player1.choice && !player2.choice) {
-      // Jika kedua pemain tidak memilih
       console.log(
         "Both players did not make a choice. It's a draw this round!"
       );
+      Swal.fire({
+        icon: "info",
+        title: "Both players did not make a choice. It's a draw this round!",
+        timer: 1000,
+      });
     } else if (!player1.choice) {
-      // Jika hanya player1 yang tidak memilih
       console.log("Player1 did not make a choice. Player2 wins this round!");
+      Swal.fire({
+        icon: "info",
+        title: "Player1 did not make a choice. Player2 wins this round!",
+        timer: 1000,
+      });
       update(ref(db, `games/${gameId}/player1`), { lives: player1.lives - 1 });
     } else if (!player2.choice) {
-      // Jika hanya player2 yang tidak memilih
       console.log("Player2 did not make a choice. Player1 wins this round!");
+      Swal.fire({
+        icon: "info",
+        title: "Player2 did not make a choice. Player1 wins this round!",
+        timer: 1000,
+      });
       update(ref(db, `games/${gameId}/player2`), { lives: player2.lives - 1 });
     } else {
-      // Jika kedua pemain memilih
       if (player1.choice === player2.choice) {
         console.log("It's a draw!");
+        Swal.fire({
+          icon: "info",
+          title: `It's a draw! They both chose ${player1.choice}`,
+          timer: 1000,
+        });
       } else if (
         (player1.choice === "rock" && player2.choice === "scissors") ||
         (player1.choice === "scissors" && player2.choice === "paper") ||
         (player1.choice === "paper" && player2.choice === "rock")
       ) {
         console.log("Player1 wins!");
+        Swal.fire({
+          icon: "info",
+          title: `Player1 wins! They chose ${player1.choice}`,
+          timer: 1000,
+        });
         update(ref(db, `games/${gameId}/player2`), {
           lives: player2.lives - 1,
         });
       } else {
         console.log("Player2 wins!");
+        Swal.fire({
+          icon: "info",
+          title: `Player2 wins! They chose ${player2.choice}`,
+          timer: 1000,
+        });
         update(ref(db, `games/${gameId}/player1`), {
           lives: player1.lives - 1,
         });
       }
     }
 
-    // Reset pilihan (choice) kedua pemain untuk ronde baru
     update(ref(db, `games/${gameId}/player1`), { choice: "" });
     update(ref(db, `games/${gameId}/player2`), { choice: "" });
 
-    // Set countdown untuk ronde baru
     setCountdown(5);
   };
 
@@ -174,9 +198,8 @@ const Game = () => {
 
         Swal.fire({
           title: isWinner ? "You won!" : "You lost!",
-          text: "The game will end in 5 seconds...",
           icon: isWinner ? "success" : "error",
-          timer: 5000,
+          timer: 7000,
         });
       }
     });
@@ -187,29 +210,44 @@ const Game = () => {
   }, [gameId, player]);
 
   const handleLeave = () => {
-    // Pastikan player ada di dalam game sebelum menghapusnya
-    if (player === "player1" || player === "player2") {
-      // Menghapus data player dari game
-      update(ref(db, `games/${gameId}/${player}`), {
-        name: "",
-        choice: "",
-        ready: false,
-        lives: 3,
-      })
-        .then(() => {
-          // Setelah pemain dihapus, navigasi ke halaman /rooms
-          navigate("/rooms");
-        })
-        .catch((error) => {
-          console.error("Error leaving game: ", error);
-        });
-    } else {
-      console.error("Player data is missing or incomplete.");
-    }
+    Swal.fire({
+      title: "Are you sure you wanna leave the room?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes!",
+      cancelButtonText: "No!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (player === "player1" || player === "player2") {
+          update(ref(db, `games/${gameId}/${player}`), {
+            name: "",
+            choice: "",
+            ready: false,
+            lives: 3,
+          })
+            .then(() => {
+              navigate("/rooms");
+            })
+            .catch((error) => {
+              console.error("Error leaving game: ", error);
+              Swal.fire({
+                icon: "error",
+                title: `Error leaving game: ${error}`,
+                timer: 1000,
+              });
+            });
+        } else {
+          console.error("Player data is missing or incomplete.");
+        }
+      }
+    });
   };
 
   return (
     <div className="bg-color3 min-h-screen flex flex-col items-center justify-center text-white">
+      <ToastContainer/>
       <h1 className="text-4xl font-bold mb-2">Suit - The Game</h1>
       <h2 className="text-xl font-bold mb-3">GameId: {gameId}</h2>
       {player && (
@@ -253,6 +291,20 @@ const Game = () => {
           </button>
         </div>
       )}
+
+      {isReady &&
+        ((!gameData?.player1?.name || !gameData?.player2?.name) ||
+        (!gameData?.player1?.ready || !gameData?.player2?.ready)) && (
+          <div className="flex space-x-4">
+            {" "}
+            <button
+              onClick={handleLeave}
+              className="bg-red-600 hover:bg-red-500 text-white font-bold py-2 px-4 rounded mb-4"
+            >
+              Leave
+            </button>
+          </div>
+        )}
 
       {gameData?.status === "ready" && (
         <h3 className="text-3xl mb-4">Countdown: {countdown}</h3>
